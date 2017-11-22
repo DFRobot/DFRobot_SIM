33386af169e2dd65b1ad95c9397d34a5a20357f3
dfrobot <ouki.wang@dfrobot.com>
2017-11-22 17:32:17 +0800

    updata client.ino

--- examples/client/client.ino
+++ examples/client/client.ino
@@ -14,25 +14,25 @@ DFSIMClient      simC;
 void setup(){
     int signalQuality;
     Serial.begin(115200);
-    sim.begin(mySerial);
+    sim.begin(mySerial);                                     //Set SoftwareSerial
     Serial.println("SIM Client");
     bool Connected = false;
     while(!Connected){
-        if(sim.init()){
+        if(sim.init()){                                      //Check and init SIMcard
             Connected = true;
         }else{
             Serial.println("Not connected");
         }
     }
     Serial.println("SIM initialized.");
-    signalQuality = simC.checkSignalQuality();
+    signalQuality = simC.checkSignalQuality();               //Check signal quality
     if(signalQuality){
         Serial.print("Signal Quality = ");
         Serial.println(signalQuality);
     }else{
         Serial.println("No Signal ! ");
     }
-    if(simC.init()){
+    if(simC.init()){                                         //Init web module
         Serial.println("Init connect server");
     }else{
         Serial.println("Failed to init connect server");
@@ -47,21 +47,21 @@ void loop(){
     readSerial(serverIP);
     Serial.print("Connect to :");
     Serial.println(serverIP);
-    if(simC.connect(serverIP, 80)){
+    if(simC.connect(serverIP, 80)){                          //Connect to server
         Serial.println("Connected!");
         Serial.println("Input data:");
         readSerial(sendData);
         Serial.print("Send data :");
         Serial.println(sendData);
-        if(simC.send(sendData)){
+        if(simC.send(sendData)){                             //Send data to server
             Serial.println("Send data, recive:");
-            Serial.println(mySerial.read());
+            Serial.println(mySerial.read());                 //Get the recive data
         }else{
             Serial.println("Failed to send");
         }
         Serial.println("Enter anything to close connect");
         readSerial(Loge);
-        if(simC.close()){
+        if(simC.close()){                                    //Close connection
             Serial.println("Close connect");
             delay(1000);
         }else{
